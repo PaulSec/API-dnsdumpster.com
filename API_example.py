3cd5937a397ed3221e8ceaa5d32078b16174ca07
PaulSec <aos.paul@gmail.com>
2015-10-01 15:15:46 +0100

    added the country in the results

--- API_example.py
+++ API_example.py
@@ -2,5 +2,4 @@ from DNSDumpsterAPI import DNSDumpsterAPI
 
 res = DNSDumpsterAPI({'verbose': True}).search('polyconseil.fr')
 for entry in res:
-	print "%s (%s) %s %s" % (entry['domain'], entry['ip'], entry['as'], entry['provider'])
-# print res
+	print "%s (%s) %s %s %s" % (entry['domain'], entry['ip'], entry['as'], entry['provider'], entry['country'])
