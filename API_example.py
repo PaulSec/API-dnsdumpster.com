b3a40ea0db9e0d2ae1b0aa9bd95500cf24f8aa22
PaulSec <aos.paul@gmail.com>
2015-10-01 13:54:47 +0100

    Updated the example

--- API_example.py
+++ API_example.py
@@ -1,4 +1,6 @@
 from DNSDumpsterAPI import DNSDumpsterAPI
 
-res = DNSDumpsterAPI({'verbose': True}).search('microsoft.com')
-print res
+res = DNSDumpsterAPI({'verbose': True}).search('polyconseil.fr')
+for entry in res:
+	print "%s (%s) %s %s" % (entry['domain'], entry['ip'], entry['as'], entry['provider'])
+# print res
