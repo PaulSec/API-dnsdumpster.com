fd9c8866f7b9a86a808942ea5b15ff3a406f2ecb
capsl <c2h@c2h.se>
2016-07-24 20:54:00 +0200

    fix domain name issue when there's a server header

--- API_example.py
+++ API_example.py
@@ -10,15 +10,15 @@ print res['domain']
 
 print "\n\n\n####### DNS Servers #######"
 for entry in res['dns_records']['dns']:
-    print("{domain} ({ip}) {as} {provider} {country}".format(**entry))
+    print("{domain} ({ip}) {as} {provider} {country} {header}".format(**entry))
 
 print "\n\n\n####### MX Records #######"
 for entry in res['dns_records']['mx']:
-    print("{domain} ({ip}) {as} {provider} {country}".format(**entry))
+    print("{domain} ({ip}) {as} {provider} {country} {header}".format(**entry))
 
 print "\n\n\n####### Host Records (A) #######"
 for entry in res['dns_records']['host']:
-    print("{domain} ({ip}) {as} {provider} {country}".format(**entry))
+    print("{domain} ({ip}) {as} {provider} {country} {header}".format(**entry))
 
 print "\n\n\n####### TXT Records #######"
 for entry in res['dns_records']['txt']:
