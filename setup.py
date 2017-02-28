4b09aefed05b1c7154b46891e96efb65f0a0f468
Paul A <aos.paul@gmail.com>
2017-02-28 18:36:11 +0100

    Files for pypi configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dnsdumpster',
+  packages = ['dnsdumpster'], # this must be the same as the name above
+  version = '0.1',
+  description = 'DNS Dumpster lib',
+  author = 'Paul Sec',
+  author_email = 'paulwebsec@gmail.com',
+  url = 'https://github.com/PaulSec/API-dnsdumpster.com',
+  download_url = 'https://github.com/PaulSec/API-dnsdumpster.comtarball/0.1',
+  keywords = ['dnsdumpster', 'dns', 'harvesting'], 
+  classifiers = [],
+)
