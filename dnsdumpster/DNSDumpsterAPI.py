0a4c43f07f4b70cf93dcf98d223410038ee87257
Paul Sec <aos.paul@gmail.com>
2017-10-24 06:04:04 +0000

    Pushed a quick fix for #14

--- dnsdumpster/DNSDumpsterAPI.py
+++ dnsdumpster/DNSDumpsterAPI.py
@@ -31,24 +31,27 @@ class DNSDumpsterAPI(object):
         for tr in trs:
             tds = tr.findAll('td')
             pattern_ip = r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'
-            ip = re.findall(pattern_ip, tds[1].text)[0]
-            domain = tds[0].text.replace('\n', '').split(' ')[0]
-            header = ' '.join(tds[0].text.replace('\n', '').split(' ')[1:])
-            reverse_dns = tds[1].find('span', attrs={}).text
+            try:
+                ip = re.findall(pattern_ip, tds[1].text)[0]
+                domain = tds[0].text.replace('\n', '').split(' ')[0]
+                header = ' '.join(tds[0].text.replace('\n', '').split(' ')[1:])
+                reverse_dns = tds[1].find('span', attrs={}).text
 
-            additional_info = tds[2].text
-            country = tds[2].find('span', attrs={}).text
-            autonomous_system = additional_info.split(' ')[0]
-            provider = ' '.join(additional_info.split(' ')[1:])
-            provider = provider.replace(country, '')
-            data = {'domain': domain,
-                    'ip': ip,
-                    'reverse_dns': reverse_dns,
-                    'as': autonomous_system,
-                    'provider': provider,
-                    'country': country,
-                    'header': header}
-            res.append(data)
+                additional_info = tds[2].text
+                country = tds[2].find('span', attrs={}).text
+                autonomous_system = additional_info.split(' ')[0]
+                provider = ' '.join(additional_info.split(' ')[1:])
+                provider = provider.replace(country, '')
+                data = {'domain': domain,
+                        'ip': ip,
+                        'reverse_dns': reverse_dns,
+                        'as': autonomous_system,
+                        'provider': provider,
+                        'country': country,
+                        'header': header}
+                res.append(data)
+            except:
+                pass
         return res
 
     def retrieve_txt_record(self, table):
@@ -117,3 +120,4 @@ class DNSDumpsterAPI(object):
             res['xls_data'] = xls_data
 
         return res
+
