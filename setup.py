059bdd20e46e312e30c426cd9a59194a999303a0
Paul A <aos.paul@gmail.com>
2017-11-02 18:57:09 +0100

    added new files and updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'dnsdumpster',
   packages = ['dnsdumpster'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'DNS Dumpster lib',
   author = 'Paul Sec',
   author_email = 'paulwebsec@gmail.com',
   url = 'https://github.com/PaulSec/API-dnsdumpster.com',
-  download_url = 'https://github.com/PaulSec/API-dnsdumpster.comtarball/0.1',
+  download_url = 'https://github.com/PaulSec/API-dnsdumpster.com/tarball/0.3',
   keywords = ['dnsdumpster', 'dns', 'harvesting'],
   install_requires=["bs4", "requests"],
   classifiers = [],
