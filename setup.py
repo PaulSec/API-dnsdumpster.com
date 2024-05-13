55f7e594aade30c0a5ea00706524e48f7aea9cb0
Paul A. <aos.paul@gmail.com>
2024-05-13 22:54:22 +0200

    Tagging new version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
+
 setup(
-  name = 'dnsdumpster',
-  packages = ['dnsdumpster'], # this must be the same as the name above
-  version = '0.8',
-  description = 'DNS Dumpster lib',
-  author = 'Paul Sec',
-  author_email = 'paulwebsec@gmail.com',
-  url = 'https://github.com/PaulSec/API-dnsdumpster.com',
-  download_url = 'https://github.com/PaulSec/API-dnsdumpster.com/tarball/0.8',
-  keywords = ['dnsdumpster', 'dns', 'harvesting'],
-  install_requires=["bs4", "requests"],
-  classifiers = [],
+    name="dnsdumpster",
+    packages=["dnsdumpster"],  # this must be the same as the name above
+    version="0.9",
+    description="DNS Dumpster lib",
+    author="Paul Sec",
+    author_email="paulwebsec@gmail.com",
+    url="https://github.com/PaulSec/API-dnsdumpster.com",
+    download_url="https://github.com/PaulSec/API-dnsdumpster.com/tarball/0.9",
+    keywords=["dnsdumpster", "dns", "harvesting"],
+    install_requires=["bs4", "requests"],
+    classifiers=[],
 )
