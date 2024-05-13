0f8ba2b8b857efbb5b80c1bcb20c30b6625c20be
Paul A. <aos.paul@gmail.com>
2024-05-13 23:19:23 +0200

    v0.10 version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="dnsdumpster",
     packages=["dnsdumpster"],  # this must be the same as the name above
-    version="0.9",
+    version="0.10",
     description="DNS Dumpster lib",
     author="Paul Sec",
     author_email="paulwebsec@gmail.com",
     url="https://github.com/PaulSec/API-dnsdumpster.com",
-    download_url="https://github.com/PaulSec/API-dnsdumpster.com/tarball/0.9",
+    download_url="https://github.com/PaulSec/API-dnsdumpster.com/tarball/0.10",
     keywords=["dnsdumpster", "dns", "harvesting"],
     install_requires=["bs4", "requests"],
     classifiers=[],
